f0a5714ebbd98939d71282108afe1a4293e989d6
DruidCat237 <aidruidvisions@gmail.com>
2024-12-05 23:55:55 +0100

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +8,334 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dancing Dock',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class FallingStar extends StatefulWidget {
+  final double startX;
+  final Color color;
+  final double angle;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const FallingStar({
+    super.key,
+    required this.startX,
+    required this.color,
+    required this.angle,
+  });
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<FallingStar> createState() => _FallingStarState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _FallingStarState extends State<FallingStar>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 600),
+      vsync: this,
+    );
+
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+
+    _controller.forward().then((_) {
+      if (mounted) {
+        setState(() {});
+      }
     });
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Positioned(
+          left: widget.startX + (widget.angle * 100 * _animation.value),
+          top: -20 + (500 * _animation.value),
+          child: Opacity(
+            opacity: 1 - _animation.value,
+            child: Icon(
+              Icons.star,
+              color: widget.color,
+              size: 20,
             ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFFF5F5F5),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              "Please Hire Me 😊",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            SizedBox(height: 40),
+            AnimatedDock(
+              items: [
+                DockItem(icon: Icons.home, label: 'Home'),
+                DockItem(icon: Icons.person, label: 'Profile'),
+                DockItem(icon: Icons.message, label: 'Messages'),
+                DockItem(icon: Icons.call, label: 'Calls'),
+                DockItem(icon: Icons.camera, label: 'Camera'),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class DockItem {
+  final IconData icon;
+  final String label;
+
+  const DockItem({required this.icon, required this.label});
+}
+
+class AnimatedDock extends StatefulWidget {
+  final List<DockItem> items;
+
+  const AnimatedDock({
+    super.key,
+    required this.items,
+  });
+
+  @override
+  State<AnimatedDock> createState() => _AnimatedDockState();
+}
+
+class _AnimatedDockState extends State<AnimatedDock>
+    with TickerProviderStateMixin {
+  late List<DockItem> _items;
+  int? _hoveredIndex;
+  bool _isDancing = false;
+  List<Widget> _stars = [];
+  late final AnimationController _danceController;
+  final _random = math.Random();
+
+  @override
+  void initState() {
+    super.initState();
+    _items = List.from(widget.items);
+    _danceController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _danceController.dispose();
+    super.dispose();
+  }
+
+  void _createStars() {
+    final colors = [
+      Colors.red,
+      Colors.blue,
+      Colors.green,
+      Colors.yellow,
+      Colors.purple,
+    ];
+
+    setState(() {
+      _stars = List.generate(5, (index) {
+        return FallingStar(
+          key: UniqueKey(),
+          startX: _random.nextDouble() * 200 + 100,
+          color: colors[index % colors.length],
+          angle: _random.nextDouble() * 2 - 1,
+        );
+      });
+
+      Future.delayed(const Duration(milliseconds: 600), () {
+        if (mounted) {
+          setState(() {
+            _stars = [];
+          });
+        }
+      });
+    });
+  }
+
+  void _startDancing() {
+    setState(() {
+      _isDancing = !_isDancing;
+    });
+
+    if (_isDancing) {
+      _danceController.repeat(reverse: true);
+      _createStars();
+    } else {
+      _danceController.stop();
+      _danceController.reset();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.2),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Wrap(
+            spacing: 8,
+            alignment: WrapAlignment.center,
+            children: [
+              ..._items.asMap().entries.map((entry) {
+                final index = entry.key;
+                final item = entry.value;
+                return _buildDraggableItem(item, index);
+              }),
+              Container(
+                width: 60,
+                height: 60,
+                margin: const EdgeInsets.symmetric(vertical: 4),
+                child: ElevatedButton(
+                  onPressed: _startDancing,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: _isDancing ? Colors.red : Colors.blue,
+                    shape: const CircleBorder(),
+                    padding: EdgeInsets.zero,
+                  ),
+                  child: const Icon(
+                    Icons.celebration,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        ..._stars,
+      ],
+    );
+  }
+
+  Widget _buildDraggableItem(DockItem item, int index) {
+    return Draggable<int>(
+      data: index,
+      feedback: Material(
+        color: Colors.transparent,
+        child: _buildIcon(item, index, true),
+      ),
+      childWhenDragging: _buildIcon(item, index, false, opacity: 0.3),
+      child: DragTarget<int>(
+        onWillAccept: (data) => data != null && data != index,
+        onAccept: (draggedIndex) {
+          setState(() {
+            final draggedItem = _items[draggedIndex];
+            _items.removeAt(draggedIndex);
+            _items.insert(index, draggedItem);
+          });
+        },
+        builder: (context, candidateData, rejectedData) {
+          return MouseRegion(
+            onEnter: (_) => setState(() => _hoveredIndex = index),
+            onExit: (_) => setState(() => _hoveredIndex = null),
+            child: _buildIcon(item, index, false),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildIcon(DockItem item, int index, bool isDragging,
+      {double opacity = 1.0}) {
+    final isHovered = _hoveredIndex == index;
+    final scale = isHovered ? 1.2 : 1.0;
+
+    return AnimatedBuilder(
+      animation: _danceController,
+      builder: (context, child) {
+        double rotation = 0.0;
+        if (_isDancing) {
+          rotation = math.sin(_danceController.value * math.pi * 2) * 0.1;
+        }
+
+        return Transform.rotate(
+          angle: rotation,
+          child: AnimatedScale(
+            scale: scale,
+            duration: const Duration(milliseconds: 200),
+            child: Opacity(
+              opacity: opacity,
+              child: Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.primaries[index % Colors.primaries.length],
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: isHovered || isDragging
+                      ? [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            blurRadius: 8,
+                            offset: const Offset(0, 4),
+                          )
+                        ]
+                      : null,
+                ),
+                child: Tooltip(
+                  message: item.label,
+                  child: Icon(
+                    item.icon,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
